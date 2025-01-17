42f8732731132148b6033229b36e34325a573020
diozenio <d.dionisiobsl@gmail.com>
2025-01-17 19:15:45 -0300

    feat: Initialize Next.js project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-8">
+      <h1 className="bg-black/[.05] dark:bg-white/[.06] text-center px-3 py-1 rounded-lg font-semibold text-2xl mb-4">
+        Orbit
+      </h1>
+      <p className="text-lg">Everything that orbits around you, centralized.</p>
+    </div>
+  );
+}
