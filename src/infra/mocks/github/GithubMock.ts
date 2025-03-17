7abd767afd2ea20fd406290aab81711f8eda8c80
diozenio <d.dionisiobsl@gmail.com>
2025-03-17 13:11:32 -0300

    refactor(mocks): Add delay to mock methods for improved simulation

--- src/infra/mocks/github/GithubMock.ts
+++ src/infra/mocks/github/GithubMock.ts
@@ -1,12 +1,14 @@
 import GithubAdapter from "@/core/interfaces/adapters/GithubAdapter";
 import GithubContributions from "@/core/domain/models/github/GithubContributions";
-
+import { delay } from "@/utils/delay";
 export class GithubMock implements GithubAdapter {
   async getContributionsSince(date: Date): Promise<GithubContributions> {
+    await delay(1000);
     return new GithubContributions(1234, date, new Date());
   }
 
   async getYearlyCommits(): Promise<number> {
+    await delay(1000);
     return 847;
   }
 }
--- src/infra/mocks/linear/LinearMock.ts
+++ src/infra/mocks/linear/LinearMock.ts
@@ -1,11 +1,14 @@
 import LinearAdapter from "@/core/interfaces/adapters/LinearAdapter";
+import { delay } from "@/utils/delay";
 
 export class LinearMock implements LinearAdapter {
   async getTasksCompletedThisWeek(): Promise<number> {
+    await delay(1000);
     return 12;
   }
 
   async getTotalPoints(): Promise<number> {
+    await delay(1000);
     return 156;
   }
 }
