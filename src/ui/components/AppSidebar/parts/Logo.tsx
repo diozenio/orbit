b32065f74a32abea521c2a512d2d51edb0445c37
diozenio <d.dionisiobsl@gmail.com>
2025-03-28 19:23:28 -0300

    fix(AppSidebar): Fix logo overflow

--- src/ui/components/AppSidebar/parts/Logo.tsx
+++ src/ui/components/AppSidebar/parts/Logo.tsx
@@ -12,12 +12,16 @@ function Logo() {
     <SidebarHeader>
       <SidebarMenu>
         <SidebarMenuItem>
-          <SidebarMenuButton size="lg" asChild>
-            <Link href="/" className="flex items-center gap-3">
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
+          <SidebarMenuButton
+            size="lg"
+            className="group-data-[state=collapsed]:bg-primary group-data-[state=collapsed]:hover:bg-primary"
+            asChild
+          >
+            <Link href="/" className="flex items-center gap-3 w-full">
+              <div className="flex aspect-square size-8 group-data-[state=collapsed]:w-full items-center justify-center rounded-lg bg-primary text-primary-foreground">
                 <Orbit className="size-4" />
               </div>
-              <div className="flex flex-col gap-0.5 leading-none">
+              <div className="flex flex-col gap-0.5 leading-none group-data-[state=collapsed]:hidden">
                 <span className="font-semibold">Orbit</span>
                 <span className="text-xs text-muted-foreground">Workspace</span>
               </div>
