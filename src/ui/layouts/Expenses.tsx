276a463b7cb1a0bb2037e9f2f03e72b6da3f3692
diozenio <d.dionisiobsl@gmail.com>
2025-03-31 19:23:49 -0300

    refactor(Expenses): Integrate data from props instead of using hard coded data

--- src/ui/layouts/Expenses.tsx
+++ src/ui/layouts/Expenses.tsx
@@ -2,52 +2,57 @@ import { Scaffold } from "@/components/Scaffold";
 import { StatsCard } from "@/components/StatsCard";
 import { i18n } from "@/i18n";
 import { formatCurrency } from "@/utils/format";
-import { CreditCard } from "lucide-react";
+import { Coins, CreditCard, HandCoins } from "lucide-react";
 import WeeklyChart from "@/components/Expenses/WeeklyChart";
 import MonthLimit from "@/components/Expenses/MonthLimit";
 import { TransactionList } from "@/components/Expenses/TransactionList";
+import { services } from "@/gateways";
 
-const chartData = [
-  { day: "Segunda", value: 100 },
-  { day: "Terça", value: 23 },
-  { day: "Quarta", value: 40 },
-  { day: "Quinta", value: 90 },
-  { day: "Sexta", value: -20 },
-  { day: "Sábado", value: 30 },
-  { day: "Domingo", value: 95 },
-];
+export default async function ExpensesLayout() {
+  const {
+    dailyExpenses,
+    totalSpent,
+    dailyLimit,
+    monthlyLimit,
+    remaining,
+    endMonth,
+    startMonth,
+    year,
+  } = await services.ExpensesService.getExpenses();
 
-export default function ExpensesLayout() {
   return (
     <Scaffold title={i18n.t("expenses.title")}>
-      <div className="grid gap-4 md:grid-cols-3 ">
+      <div className="grid gap-4 grid-cols-1 @2xl/main:grid-cols-3">
         <StatsCard
           title={i18n.t("expenses.cards.total")}
           description={i18n.t("expenses.cards.totalDescription")}
           icon={CreditCard}
-          value={formatCurrency(238.23, "BRL")}
+          value={formatCurrency(totalSpent, "BRL")}
         />
         <StatsCard
           title={i18n.t("expenses.cards.remaining")}
           description={i18n.t("expenses.cards.remainingDescription")}
-          icon={CreditCard}
-          value={formatCurrency(2141.77, "BRL")}
+          icon={HandCoins}
+          value={formatCurrency(remaining, "BRL")}
         />
         <StatsCard
           title={i18n.t("expenses.cards.limit")}
           description={i18n.t("expenses.cards.limitDescription")}
-          icon={CreditCard}
-          value={formatCurrency(2500, "BRL")}
+          icon={Coins}
+          value={formatCurrency(monthlyLimit, "BRL")}
         />
       </div>
       <div className="grid gap-4 md:grid-cols-2">
-        <WeeklyChart dailyLimit={100} chartData={chartData} />
+        <WeeklyChart
+          dailyLimit={dailyLimit}
+          dailyExpenses={dailyExpenses.map((expense) => expense.toJSON())}
+        />
         <MonthLimit
-          limit={2500}
-          spent={1200}
-          year={"2024"}
-          startMonth="January"
-          endMonth="February"
+          limit={monthlyLimit}
+          spent={totalSpent}
+          year={year}
+          startMonth={startMonth}
+          endMonth={endMonth}
         />
       </div>
       <div className="w-full">
