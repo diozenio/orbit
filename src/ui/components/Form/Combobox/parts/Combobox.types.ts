991ebed5b3584ebe7bcaa579f5192fdfd10f6b16
diozenio <d.dionisiobsl@gmail.com>
2025-04-14 16:45:05 -0300

    refactor(ui): Change icon type in ComboboxItem from LucideIcon to string

--- src/ui/components/Form/Combobox/parts/Combobox.types.ts
+++ src/ui/components/Form/Combobox/parts/Combobox.types.ts
@@ -1,7 +1,5 @@
-import { LucideIcon } from "lucide-react";
-
 export type ComboboxItem = {
   value: string;
   label: string;
-  icon: LucideIcon;
+  icon: string;
 };
--- src/ui/components/Form/Combobox/parts/Item.tsx
+++ src/ui/components/Form/Combobox/parts/Item.tsx
@@ -1,16 +1,18 @@
 import { cn } from "@/lib/utils";
-import { CommandItem } from "@/ui/primitives/command";
+import { CommandItem } from "@/primitives/command";
 import { ComboboxItem } from "./Combobox.types";
+import { Icon } from "@/primitives/icon";
 
 interface ItemProps extends ComboboxItem {
   selected?: boolean;
   onSelect?: (value: string) => void;
 }
 
-function Item({ icon: Icon, label, value, onSelect, selected }: ItemProps) {
+function Item({ icon, label, value, onSelect, selected }: ItemProps) {
   return (
     <CommandItem key={value} value={value} onSelect={onSelect}>
       <Icon
+        name={icon}
         className={cn("h-4 w-4", selected ? "opacity-100" : "opacity-40")}
       />
       <span>{label}</span>
