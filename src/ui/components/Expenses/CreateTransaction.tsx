6fa8cb3617f2f307ff23121dc92aad36585c18d6
diozenio <d.dionisiobsl@gmail.com>
2025-04-15 20:23:23 -0300

    feat(CreateTransaction): Implement form validation and refactor to use Form component

--- src/ui/components/Expenses/CreateTransaction.tsx
+++ src/ui/components/Expenses/CreateTransaction.tsx
@@ -10,12 +10,16 @@ import {
   DialogFooter,
 } from "@/ui/primitives/dialog";
 import { CirclePlus } from "lucide-react";
-import MoneyInput from "./MoneyInput";
-import { TextInput } from "../Form/TextInput";
 import { useTransactions } from "@/hooks/useTransactions";
-import { Combobox } from "../Form/Combobox";
-import TransactionCategory from "@/models/expenses/TransactionCategory";
-import { ComboboxItem } from "../Form/Combobox/parts";
+import { z } from "zod";
+import { useCreateTransaction } from "@/hooks/useCreateTransaction";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useQueryClient } from "@tanstack/react-query";
+import Transaction from "@/core/domain/models/expenses/Transaction";
+import { Form } from "../Form";
+import TransactionCategory from "@/core/domain/models/expenses/TransactionCategory";
+import { ComboboxItem } from "../Form/Inputs/Combobox";
 
 function getComboboxItems(categories?: TransactionCategory[]): ComboboxItem[] {
   return (
@@ -27,10 +31,44 @@ function getComboboxItems(categories?: TransactionCategory[]): ComboboxItem[] {
   );
 }
 
-function CreateTransaction() {
-  const { categories } = useTransactions();
+const schema = z.object({
+  title: z
+    .string({ required_error: "Title is required" })
+    .max(50, "Title must be less than 50 characters"),
+  amount: z.coerce
+    .number({
+      required_error: "Amount is required",
+      invalid_type_error: "Please enter a valid amount",
+    })
+    .refine((val) => val !== 0, {
+      message: "Amount must be different from 0",
+    }),
+  transactionCategoryId: z
+    .string({ required_error: "Please select a category" })
+    .uuid("Invalid transaction category ID"),
+});
 
+type FormData = z.infer<typeof schema>;
+
+function CreateTransaction() {
+  const queryClient = useQueryClient();
+  const { categories } = useTransactions();
+  const { mutate: createTransaction } = useCreateTransaction();
   const comboboxItems = getComboboxItems(categories);
+
+  const form = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: FormData) => {
+    const transaction = Transaction.fromForm(data);
+
+    createTransaction([transaction], {
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ["expenses"] });
+      },
+    });
+  };
 
   return (
     <Dialog>
@@ -40,8 +78,9 @@ function CreateTransaction() {
           {i18n.t("expenses.actions.createTransaction.trigger")}
         </Button>
       </DialogTrigger>
+
       <DialogContent className="sm:max-w-[600px]">
-        <form className="contents">
+        <Form.Root form={form} onSubmit={form.handleSubmit(onSubmit)}>
           <DialogHeader>
             <DialogTitle>
               {i18n.t("expenses.actions.createTransaction.dialog.title")}
@@ -50,9 +89,10 @@ function CreateTransaction() {
               {i18n.t("expenses.actions.createTransaction.dialog.description")}
             </DialogDescription>
           </DialogHeader>
-          <div className="space-y-4 py-4">
-            <TextInput
-              id="title"
+
+          <div className="py-6 space-y-4">
+            <Form.TextInput
+              name="title"
               label={i18n.t(
                 "expenses.actions.createTransaction.dialog.inputs.title.label"
               )}
@@ -60,28 +100,30 @@ function CreateTransaction() {
                 "expenses.actions.createTransaction.dialog.inputs.title.placeholder"
               )}
             />
-            <MoneyInput
-              id="amount"
+            <Form.MoneyInput
+              name="amount"
               label={i18n.t(
                 "expenses.actions.createTransaction.dialog.inputs.amount.label"
               )}
             />
-            <Combobox
-              items={comboboxItems}
+            <Form.Combobox
               label={i18n.t(
                 "expenses.actions.createTransaction.dialog.inputs.combobox.label"
               )}
               placeholder={i18n.t(
                 "expenses.actions.createTransaction.dialog.inputs.combobox.placeholder"
               )}
+              name="transactionCategoryId"
+              items={comboboxItems}
             />
           </div>
+
           <DialogFooter>
             <Button type="submit">
               {i18n.t("expenses.actions.editExpenses.dialog.submit")}
             </Button>
           </DialogFooter>
-        </form>
+        </Form.Root>
       </DialogContent>
     </Dialog>
   );
